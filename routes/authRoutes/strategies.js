ec2e512aa03fc726b9f3374bef27e661e0c54c9e
Adam Carter <adam.clayton.carter@gmail.com>
2021-07-07 12:44:03 -0400

    Add linkedin auth

--- routes/authRoutes/strategies.js
+++ routes/authRoutes/strategies.js
@@ -2,5 +2,6 @@ module.exports = [
   { name: 'google', scope: ['profile', 'email'] },
   { name: 'facebook', scope: 'email' },
   { name: 'twitter' },
-  { name: 'linkedin', scope: ['r_basicprofile', 'r_emailaddress'] },
+  // { name: 'linkedin', scope: ['r_basicprofile', 'r_emailaddress'] },
+  { name: 'linkedin', scope: ['r_emailaddress', 'r_liteprofile'] },
 ];
